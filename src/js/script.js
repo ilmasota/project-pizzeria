8329a948eb139c5afe8b0f2d8e43794d426831bd
Gosia Marczak <uroshirvisoittaa@gmail.com>
2020-09-08 14:40:55 +0200

    Add fetch and data parsing to App.initData function

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
 
   const templates = {
@@ -513,13 +518,31 @@
 
       // instances of class Product
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function(){
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          //console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu() method */
+          thisApp.initMenu();
+        });
+
+      //console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function(){
@@ -538,7 +561,7 @@
 
       thisApp.initCart();
       thisApp.initData();
-      thisApp.initMenu();
+      //thisApp.initMenu();
 
     },
   };
